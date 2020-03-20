24400cbcd8ab4ecb5f9aca118115b6df9c8156d2
exoconnor <connor@exoconnor.com>
2020-03-20 00:16:39 -0700

    p65

--- src/four_clojure/core.clj
+++ src/four_clojure/core.clj
@@ -127,3 +127,26 @@
      {}
      xs)))
 
+;; Clojure has many sequence types, which act in subtly different ways. The core
+;; functions typically convert them into a uniform "sequence" type and work with
+;; them that way, but it can be important to understand the behavioral and
+;; performance differences so that you know which kind is appropriate for your
+;; application.
+
+;; Write a function which takes a collection and returns one of :map, :set, :list,
+;; or :vector - describing the type of collection it was given. You won't be
+;; allowed to inspect their class or use the built-in predicates like list? - the
+;; point is to poke at them and understand their behavior.
+;; NOTE - can't use class, type, Class, vector?, sequential?, list?, seq?, map?, set?, instance?, getClass
+(def p65
+  (fn [coll]
+    (let [a (gensym)
+          b (gensym)
+          test-case (conj (empty coll) [a b] [a a] [a a])
+          test-size (count test-case)]
+      (cond
+        (= 1 test-size) :map
+        (= 2 test-size) :set
+        (= (first test-case) [a a]) :list
+        :else :vector))))
+
