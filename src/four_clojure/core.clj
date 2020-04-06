96d0ebd621dedeeb959a8b585d1632b71ed6b047
exoconnor <connor@exoconnor.com>
2020-04-06 00:06:32 -0700

    p79, p80, p81

--- src/four_clojure/core.clj
+++ src/four_clojure/core.clj
@@ -312,10 +312,55 @@
 ;; should start at the top of the triangle and move to an adjacent number on the
 ;; next row until the bottom of the triangle is reached.
 (def p79
-  (fn tmp [triangle]
+  (fn [triangle]
     (apply min
            (reduce (fn [path-sums row]
-                     (map (fn [[]]
-                            ())))))
-    (let [min-path-sum (fn [head])]
-     )))
+                     (map (fn [from to] (+ (apply min from) to))
+                          (partition 2 1 (concat [(first path-sums)]
+                                                 path-sums
+                                                 [(last path-sums)]))
+                          row))
+                   triangle))))
+
+;; A number is "perfect" if the sum of its divisors equal the number itself. 6
+;; is a perfect number because 1+2+3=6. Write a function which returns true for
+;; perfect numbers and false otherwise.
+(def p80
+  (fn [n]
+    (->> (inc (int (/ n 2)))
+         (range 1)
+         (filter #(zero? (mod n %)))
+         (reduce +)
+         (= n))))
+
+;; Write a function which returns the intersection of two sets. The intersection
+;; is the sub-set of items that each set has in common.
+(def p81
+  (fn [s1 s2]
+    (reduce (fn [intersect el]
+              (if (contains? s2 el)
+                (conj intersect el)
+                intersect))
+            #{}
+            s1)))
+;; (comp set filter) works and is pretty dope -> use one set as a function, the other as
+;; a sequence for the filter function, then cast back to set.
+
+;; A word chain consists of a set of words ordered so that each word differs by
+;; only one letter from the words directly before and after it. The one letter
+;; difference can be either an insertion, a deletion, or a substitution. Here is
+;; an example word chain: cat -> cot -> coat -> oat -> hat -> hot -> hog -> dog
+;; Write a function which takes a sequence of words, and returns true if they
+;; can be arranged into one continous word chain, and false if they cannot.
+(def p82
+  (fn [strings]
+    ;; Brute force/recursive search? Levenshtein distance 1 sequence
+    "DR STUB"))
+
+;; Write a function which takes a variable number of booleans. Your function
+;; should return true if some of the parameters are true, but not all of the
+;; parameters are true. Otherwise your function should return false.
+(def p83
+  (fn [& xs]
+    (let [truthiness (reduce (fn [c x] (if x (inc c) c)) 0 xs)]
+      (and (< 0 truthiness) (not= truthiness (count xs))))))
